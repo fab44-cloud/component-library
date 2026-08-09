239ab1a2ec10010042176c2f7dd76da84568a1f6
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-08-08 19:37:40 -0600

    Import badge component

--- index.jsx
+++ index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Menu from "./components/Menu/index"
 import Star from "./components/Star"
+import Badge from "./components/Badge/Badge.jsx"
 
 function App() {
   return (
-    <h1>Your components go here</h1>
+    <Badge>Success</Badge>
   )
 }
 
