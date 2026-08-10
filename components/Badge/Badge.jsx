43930a9a2168e92a75315ccaafe295b9e8958d60
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-08-09 21:05:24 -0600

    Accept a color prop for the badge

--- components/Badge/Badge.jsx
+++ components/Badge/Badge.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 
-export default function Badge({ children }) {
+export default function Badge({ children, color="gray" }) {
+    const validColors = ["gray", "red", "yellow", "green", "blue", "indigo", "purple", "pink"]
+    
+    const badgeColor = validColors.includes(color) ? color : "gray"
+
+    const badgeClass = `badge badge-${badgeColor}`
+
     return (
-        children
+        <span className={badgeClass}>
+            {children}
+        </span>
     )
 }
