7cfe8d5a03727a0b7c05e6ddcfbdfc41e5c90038
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-08-11 22:05:15 -0600

    Utilize badge shape property for styles

--- components/Badge/Badge.jsx
+++ components/Badge/Badge.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 
-export default function Badge({ children, color="gray" }) {
+export default function Badge({ children, color="gray", shape="square" }) {
     const validColors = ["gray", "red", "yellow", "green", "blue", "indigo", "purple", "pink"]
     
     const badgeColor = validColors.includes(color) ? color : "gray"
+    const badgeShape = shape === "pill" ? "pill" : "square"
 
-    const badgeClass = `badge badge-${badgeColor}`
+    const badgeClass = `badge badge-${badgeColor} badge-${badgeShape}`
 
     return (
         <span className={badgeClass}>
