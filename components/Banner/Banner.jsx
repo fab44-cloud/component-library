11a5d1023818b2655da3ae8edfd997c0eaaa463f
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-08-13 21:45:27 -0600

    Create helper function for the banner title

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -8,10 +8,17 @@ export default function Banner({ children, status="neutral" }) {
         <div className={`banner banner-${bannerStatus}`}>
             <div className="banner-icon-container">
                 {status === "success" && "✅"}
+                {status === "warning" && "⚠️"}
+                {status === "error" && "❌"}
+                {status === "neutral" && "ℹ️"}
             </div>
             <div className="banner-content">
                 {children}
             </div>
         </div>
     )
+}
+
+export function BannerTitle({ children }) {
+    return <h4>{children}</h4>
 }
